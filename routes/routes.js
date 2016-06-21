f60f1473b7e3146640d1c10080983a9a171ffcd4
Jhong <erpaigan@gmail.com>
2016-06-21 13:10:15 +0800

    google verification fix

--- routes/routes.js
+++ routes/routes.js
@@ -100,6 +100,15 @@ module.exports = function(app, passport, transporter) {
 								subject: 'Join Migo',
 								text: 'Servus! You may use this invitation code: ' + newCode + ' along with this email address to join. Enjoy your stay.'
 							};
+							
+							// verify connection configuration
+							transporter.verify(function(error, success) {
+							   if (error) {
+									console.log(error);
+							   } else {
+									console.log('Server is ready to take our messages');
+							   }
+							});
 
 							transporter.sendMail(mailOptions, function(error, response) {
 								if (error) { 
